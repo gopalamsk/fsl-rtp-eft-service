6201be2d3ae061afbcd93159d1f657d697286d78
gopalamsk <gopalamsk@gmail.com>
2026-09-20 17:55:20 -0400

    Align ShedLock with FSL cache datasource and payment schema

--- src/main/java/com/bns/fsl/eft/config/ShedLockConfiguration.java
+++ src/main/java/com/bns/fsl/eft/config/ShedLockConfiguration.java
@@ -3,6 +3,7 @@ package com.bns.fsl.eft.config;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,11 +15,14 @@ import javax.sql.DataSource;
 public class ShedLockConfiguration {
 
     @Bean
-    LockProvider lockProvider(DataSource dataSource) {
+    LockProvider lockProvider(
+            @Qualifier("fslCacheDataSource") DataSource fslCacheDataSource) {
+
         return new JdbcTemplateLockProvider(
                 JdbcTemplateLockProvider.Configuration.builder()
-                        .withJdbcTemplate(new JdbcTemplate(dataSource))
+                        .withJdbcTemplate(new JdbcTemplate(fslCacheDataSource))
                         .usingDbTime()
+                        .withTableName("payment.shedlock")
                         .build());
     }
 }
