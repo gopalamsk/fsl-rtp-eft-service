339165ee105eb99f2451ee0d204e7f6107e3669d
gopalamsk <gopalamsk@gmail.com>
2026-09-20 01:24:07 -0400

    feat: add batch scheduling and ShedLock configuration

---
+++ src/main/java/com/bns/fsl/eft/config/ShedLockConfiguration.java
@@ -0,0 +1,24 @@
+package com.bns.fsl.eft.config;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableSchedulerLock(defaultLockAtMostFor = "PT1H")
+public class ShedLockConfiguration {
+
+    @Bean
+    LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(
+                JdbcTemplateLockProvider.Configuration.builder()
+                        .withJdbcTemplate(new JdbcTemplate(dataSource))
+                        .usingDbTime()
+                        .build());
+    }
+}
