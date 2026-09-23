34c8e5d2235004647fa9388c189676ad9ec35702
gopalamsk <gopalamsk@gmail.com>
2026-09-22 22:24:03 -0400

    Remove superseded EFT batch class location

--- src/main/java/com/bns/fsl/eft/model/EftDecisionResult.java
+++
@@ -1,8 +0,0 @@
-package com.bns.fsl.eft.model;
-
-public record EftDecisionResult(
-        PendingEftDecisionProjection source,
-        String terminalStatus,
-        String decision,
-        boolean autoApproved) {
-}
